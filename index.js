7da2a619239da0fd81316c7001cf46eb29809710
paxarpp <pavelpp1974@gmail.com>
2019-03-24 18:43:00 +0300

    crud begin

--- index.js
+++ index.js
@@ -1,20 +1,64 @@
 const express = require('express');
+var bodyParser = require('body-parser')
+
 const app = express();
 const port = 3000;
+const users = require('./users');
 
-app.get('/', (_request, response) => {
-    response.send('Hello from node server')
+app.use(bodyParser.json());
+
+app.get('/users', (_request, response) => {
+    response.send(users)
 });
 
-app.get('/:name', (request, response) => {
-    console.log(request.url)
-    response.send(`${request.params.name}, Hello from node server`)
+app.get('/user/:id', (request, response) => {
+    const { id } = request.params;
+    const user = users.find(user => user.id === id);
+    if (!user) {
+        response.send(`user with id: ${id} not found`)
+    } else {
+        response.send(user)
+    }
+});
+
+app.delete('/user/:id', (request, response) => {
+    const { id } = request.params;
+    const index = users.findIndex(user => user.id === id);
+    if (index === -1) {
+        response.send(`user with id: ${id} not found`)
+    } else {
+        const user = users.splice(index, 1);
+        response.send(...user)
+    }
+});
+
+app.post('/user', (request, response) => {
+    const user = request.body;
+    id = +users.reduce((acc, user) => {
+        if (+user.id > +acc) {
+            acc = user.id
+        }
+        return acc;
+    }, 1);
+    user.id = (id + 1).toString();
+    users.push(user);
+    response.send(user)
+
+});
+
+app.get('/:type', (request, response) => {
+    const { type } = request.params;
+    const types = users.filter(user => user.type === type);
+    if (types.length === 0) {
+        response.send(`${types} not found`)
+    } else {
+        response.send(types)
+    }
 });
 
 app.listen(port, (err) => {
     if (err) {
-        return console.log(`somefing wrong`, err)
+        return console.log(`something wrong`, err)
     }
-
     console.log(`server running on port ${port}`)
 });
---
+++ users.js
@@ -0,0 +1,18 @@
+let users = [
+    {
+        name: 'Luck',
+        id: '1',
+        lastname: 'Skyworker',
+        age: 25,
+        type: 'jadai',
+    },
+    {
+        id: '2',
+        name: 'Khan',
+        lastname: 'Solo',
+        age: 35,
+        type: 'pilot',
+    },
+];
+
+module.exports = users;
