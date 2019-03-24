e4b7233521dc378f3995343926cff500abe9886b
paxarpp <pavelpp1974@gmail.com>
2019-03-24 16:13:26 +0300

    get value req

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (request, response) => {
+app.get('/', (_request, response) => {
     response.send('Hello from node server')
+});
+
+app.get('/:name', (request, response) => {
+    console.log(request.url)
+    response.send(`${request.params.name}, Hello from node server`)
 });
 
 app.listen(port, (err) => {
